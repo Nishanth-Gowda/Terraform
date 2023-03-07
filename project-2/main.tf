d051c2b8d95153dedb120542c152c079ffb7a704
Nishanth-Gowda <vishu1332000@gmail.com>
2023-03-07 12:44:55 +0530

    Added Optimized Code

--- project-2/main.tf
+++ project-2/main.tf
@@ -1,165 +1,126 @@
+# Create VPC Resources
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
   tags = {
-    Name = var.vpc_tag
+    Name = "${var.environment}-vpc"
   }
 }
-
 resource "aws_subnet" "subnet" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.subnet_cidr
   map_public_ip_on_launch = true
-
   tags = {
-    Name = var.subnet_tag
+    Name = "${var.environment}-subnet"
   }
 }
-
 resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.vpc.id
   tags = {
-    Name = var.internet_gateway_tag
+    Name = "${var.environment}-internet-gateway"
   }
 }
-
 resource "aws_default_route_table" "default_route" {
   default_route_table_id = aws_vpc.vpc.default_route_table_id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.internet_gateway.id
   }
 }
-
+# Create S3 Bucket and IAM Policies
+resource "aws_iam_role" "ec2_iam_role" {
+  name               = "${var.environment}-ec2-iam-role"
+  assume_role_policy = var.ec2-trust-policy
+}
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "${var.environment}-ec2-instance-profile"
+  role = aws_iam_role.ec2_iam_role.id
+}
+resource "aws_iam_role_policy" "ec2_role_policy" {
+  name   = "${var.environment}-ec2-role-policy"
+  role   = aws_iam_role.ec2_iam_role.id
+  policy = var.ec2-s3-permissions
+}
+resource "aws_s3_bucket" "s3" {
+  bucket        = var.bucket_name
+  force_destroy = true
+  tags = {
+    Name = "${var.environment}-s3-bucket"
+  }
+}
+# Obtain User's Local Public IP
 data "external" "myipaddr" {
   program = ["bash", "-c", "curl -s 'https://ipinfo.io/json'"]
 }
-
+# Create EC2 Security Group and Security Rules
 resource "aws_security_group" "jenkins_security_group" {
-  name_prefix = var.security_group_name
-  description = "Jenkins Security Group"
+  name        = "${var.environment}-jenkins-security-group"
+  description = "Apply to Jenkins EC2 instance"
   vpc_id      = aws_vpc.vpc.id
-
   ingress {
-    description = "Allow SSH from my Public IP"
+    description = "Allow SSH from MY Public IP"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${data.external.myipaddr.result.ip}/32"]
   }
-
   ingress {
-    description = "Allows Access to the Jenkins Server"
+    description = "Allow access to Jenkis from My IP"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${data.external.myipaddr.result.ip}/32"]
   }
-
-  ingress {
-    description = "Allows Access to the Jenkins Server"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
+    description = "Allow All Outbound"
+    protocol    = -1
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "Jenkins_Security_Group"
+    Name = "${var.environment}-jenkins-security-group"
   }
 }
-
-resource "aws_iam_role" "ec2_role" {
-  name = var.ec2_role_name
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
+# Lookup Amazon Linux Image
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
 }
-
-resource "aws_iam_policy" "ec2_role_policy" {
-  name = var.ec2_role_policy_name
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "s3:Get*",
-          "s3:List*",
-          "dynamodb:ListTables"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
-  })
+# Create SSH Keys for EC2 Remote Access
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
 }
-
-resource "aws_iam_role_policy_attachment" "ec2_role_policy_attachment" {
-  policy_arn = aws_iam_policy.ec2_role_policy.arn
-  role       = aws_iam_role.ec2_role.name
+resource "local_file" "private_key_pem" {
+  content         = tls_private_key.generated.private_key_pem
+  filename        = "${var.ssh_key}.pem"
+  file_permission = "0400"
 }
-
-resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = var.ec2_instance_profile_name
-  role = aws_iam_role.ec2_role.name
+resource "aws_key_pair" "generated" {
+  key_name   = var.ssh_key
+  public_key = tls_private_key.generated.public_key_openssh
 }
-
-resource "aws_instance" "ec2_instance" {
-  ami                  = var.ami
+# Create EC2 Instance
+resource "aws_instance" "jenkins_server" {
+  ami                  = data.aws_ami.amazon_linux_2.id
   instance_type        = var.instance_type
-  key_name             = var.ssh_key_name
-  security_groups      = [aws_security_group.jenkins_security_group.id]
+  key_name             = aws_key_pair.generated.key_name
   subnet_id            = aws_subnet.subnet.id
-  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+  security_groups      = [aws_security_group.jenkins_security_group.id]
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.id
   user_data            = var.ec2_user_data
+  connection {
+    user        = "ec2-user"
+    private_key = tls_private_key.generated.private_key_pem
+    host        = self.public_ip
+  }
   tags = {
-    Name = var.ec2_tag
+    Name = "${var.environment}-jenkins-server"
   }
-}
-
-resource "aws_s3_bucket" "s3" {
-  bucket = var.bucket_name
-
-  tags = {
-    Name = var.bucket_name
-  }
-}
-
-resource "aws_s3_bucket_acl" "jenkinsbucketacl" {
-  bucket = var.bucket_name
-  acl    = "private"
-}
-
-resource "aws_dynamodb_table" "ec2-instance-demo-table" {
-  name = var.dynamo_table_name
-  billing_mode = "PROVISIONED"
-  read_capacity = 10
-  write_capacity = 10
-  hash_key = "UserID"
-
-  attribute {
-    name = "UserID"
-    type = "S"
-  }
-
-  tags = {
-    Name        = var.dynamodb_tag
-    Environment = var.dynamodb_env
-  }
-
 }
--- project-2/vars.tf
+++ project-2/vars.tf
@@ -1,104 +1,107 @@
-# This file references the resources and sets the necessary variables.
+variable "environment" {
+  description = "Environment name for deployment"
+  type        = string
+  default     = "demo"
+}
+variable "aws_region" {
+  description = "AWS region resources are deployed to"
+  type        = string
+  default     = "us-east-1"
+}
+# VPC Variables
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "VPC cidr block"
+  type        = string
   default     = "10.0.0.0/16"
 }
-
-variable "vpc_tag" {
-  description = "Tag for the VPC"
-  default     = "my-vpc"
-}
-
 variable "subnet_cidr" {
-  description = "CIDR block for the subnet"
+  description = "Subnet cidr block"
+  type        = string
   default     = "10.0.0.0/24"
 }
-
-variable "subnet_tag" {
-  description = "Tag for the subnet"
-  default     = "my-subnet"
-}
-
-variable "internet_gateway_tag" {
-  description = "Tag for the Internet Gateway"
-  default     = "my-igw"
-}
-
-variable "ec2_role_name" {
-  description = "Name of the EC2 instance role"
-  default     = "ec2_role"
-}
-
-variable "ec2_instance_profile_name" {
-  description = "Name of the EC2 instance profile"
-  default     = "ec2_instance_profile"
-}
-
-variable "ec2_role_policy_name" {
-  description = "Name of the policy attached to the EC2 instance role"
-  default     = "ec2_role_policy"
-}
-
+# IAM Role Variables
 variable "ec2-trust-policy" {
-  description = "Name of the JSON file containing the trust policy for the EC2 instance role"
-  default     = "trust-policy.json"
+  description = "sts assume role policy for EC2"
+  type        = string
+  default     = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Principal": {
+                "Service": [
+                    "ec2.amazonaws.com"
+                ]
+            }
+        }   
+    ]
 }
-
+EOF  
+}
 variable "ec2-s3-permissions" {
-  description = "Name of the JSON file containing the S3 bucket permissions for the EC2 instance role"
-  default     = "s3-permissions.json"
+  description = "IAM permissions for EC2 to S3"
+  type        = string
+  default     = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket",
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:DeleteObject"
+            ],
+            "Resource": "*"
+        }
+    ]
 }
-
+EOF
+}
+# S3 Variables
 variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  default     = "rayblackteam"
+  description = "S3 bucket name"
+  type        = string
+  default     = "terraform1demo1s3bucket2023-nish"
 }
-
-variable "security_group_name" {
-  description = "Name of the security group"
-  default     = "my-security-group"
+# EC2 Variables
+variable "ssh_key" {
+  description = "ssh key name"
+  type        = string
+  default     = "my-ssh-key"
 }
-
-variable "ami" {
-  description = "AMI ID for the EC2 instance"
-  default     = "ami-0dfcb1ef8550277af"
-}
-
 variable "instance_type" {
-  description = "Type of the EC2 instance"
+  description = "Jenkins EC2 instance type"
+  type        = string
   default     = "t2.micro"
 }
-
-variable "ssh_key_name" {
-  description = "Name of the SSH key pair"
-  default     = "MyNewKeyPair"
-}
-
 variable "ec2_user_data" {
-  description = "User data script for EC2 instances"
+  description = "User data shell script for Jenkins EC2"
   type        = string
   default     = <<EOF
 #!/bin/bash
-# Install Jenkins and Java
-sudo wget -O /etc/yum.repos.d/jenkins.repo \\
-    <https://pkg.jenkins.io/redhat-stable/jenkins.repo>
-sudo rpm --import <https://pkg.jenkins.io/redhat-stable/jenkins.io.key>
-sudo yum -y upgrade
+# Install Jenkins and Java 
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade -y
 # Add required dependencies for the jenkins package
-sudo amazon-linux-extras install -y java-openjdk11
+sudo amazon-linux-extras install -y java-openjdk11 
 sudo yum install -y jenkins
 sudo systemctl daemon-reload
-
 # Start Jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
-
 # Firewall Rules
 if [[ $(firewall-cmd --state) = 'running' ]]; then
     YOURPORT=8080
     PERM="--permanent"
     SERV="$PERM --service=jenkins"
-
     firewall-cmd $PERM --new-service=jenkins
     firewall-cmd $SERV --set-short="Jenkins ports"
     firewall-cmd $SERV --set-description="Jenkins port exceptions"
@@ -108,22 +111,4 @@ if [[ $(firewall-cmd --state) = 'running' ]]; then
     firewall-cmd --reload
 fi
 EOF
-}
-
-variable "ec2_tag" {
-  description = "Tag for the EC2 instance"
-  default     = "jenkins-server"
-}
-
-variable "dynamo_table_name" {
-  description = "Table Name for  EC2 instance access"
-  default = "MyTable"
-}
-
-variable "dynamodb_tag" {
-  default = "dynamodb-table"
-}
-
-variable "dynamodb_env" {
-  default = "test"
 }
